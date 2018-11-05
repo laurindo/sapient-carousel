61829c2cfb3915ff0918736fd1464051618cc9e1
Daniel Laurindo <dslaurindo@gmail.com>
2018-11-05 16:53:49 -0300

    #2 Simulating a simple carousel with pure CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import './slider';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className='wrap'>
+          <ul className='carousel is-set'>
+            <li className='carousel-seat'>
+              <h2>1</h2>
+            </li>
+            <li className='carousel-seat'>
+              <h2>2</h2>
+            </li>
+            <li className='carousel-seat'>
+              <h2>3</h2>
+            </li>
+            <li className='carousel-seat'>
+              <h2>4</h2>
+            </li>
+            <li className='carousel-seat'>
+              <h2>5</h2>
+            </li>
+            <li className='carousel-seat is-ref'>
+              <h2>6</h2>
+            </li>
+          </ul>
+        </div>
+        <div className='controls'>
+          <button className='toggle'>Prev</button>
+          <button className='toggle' data-toggle='next'>Next</button>
+        </div>
       </div>
     );
   }
